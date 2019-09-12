ed3dacbf2c52fd1fbefa3e04e95f4a4ae6ccf712
Ivan Doroshenko <idoroshenko@lohika.com>
2019-09-12 17:37:37 +0300

    fix

--- fetch/src/amqp.ts
+++ fetch/src/amqp.ts
@@ -6,7 +6,7 @@ export async function getConnection(url: string): Promise<Connection> {
     try {
         return await connect(url)
     } catch (e) {
-        log.warning(`connection error, retry in ${retryTimeout}`)
+        log.warn(`connection error, retry in ${retryTimeout}`)
         await new Promise(resolve => setTimeout(resolve, retryTimeout))
         return getConnection(url)
     }
